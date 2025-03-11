e815386d8d44bbc9de674e75d078bfd5fc41d114
emeraldl7 <li.weiming930@gmail.com>
2025-03-11 20:33:23 +0900

    changed button names

--- src/pages/Schedule/ScheduleInput.tsx
+++ src/pages/Schedule/ScheduleInput.tsx
@@ -135,7 +135,7 @@ const ScheduleInput: React.FC = () => {
               onClick={handleSubmit}
               className="inline-flex items-center rounded-md border border-transparent bg-blue-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
             >
-              'スケジュール提出'
+              スケジュール提出
             </button>
           </div>
         </div>
